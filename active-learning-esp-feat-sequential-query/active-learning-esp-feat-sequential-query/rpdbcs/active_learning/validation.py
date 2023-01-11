48be46fc7e2d6e1bd5e422e594167a6323b46c8c
Luis Câmara <luisefcamara@gmail.com>
2023-01-11 19:16:26 +0000

    CheckType of X

--- active-learning-esp-feat-sequential-query/active-learning-esp-feat-sequential-query/rpdbcs/active_learning/validation.py
+++ active-learning-esp-feat-sequential-query/active-learning-esp-feat-sequential-query/rpdbcs/active_learning/validation.py
@@ -470,6 +470,7 @@ def main(config, D, config_path):
     X = np.expand_dims(D.asMatrix()[:, :6100], axis=1)  # Transforms shape (n,10800) to (n,1,6100).
     Y, Ynames = D.getMulticlassTargets()
     print(D.asDataFrame()["bcs name"].value_counts())
+    print("Type:", type(X))
     # Yset = enumerate(set(Y))
     # Y, Ymap = pd.factorize(Y)
     # Ynames = {i: Ynames[oldi] for i, oldi in enumerate(Ymap)}
