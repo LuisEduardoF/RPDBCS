6ef90ef1cdeda1920ecb26b82ac84456003b52a6
Luis Câmara <luisefcamara@gmail.com>
2023-01-12 02:21:13 +0000

    2 try

--- active-learning-esp-feat-sequential-query/active-learning-esp-feat-sequential-query/rpdbcs/active_learning/validation.py
+++ active-learning-esp-feat-sequential-query/active-learning-esp-feat-sequential-query/rpdbcs/active_learning/validation.py
@@ -472,6 +472,8 @@ def do_methods(X, Y, Ynames, X0, Y0, Xpool, Ypool, Xtest, Ytest, save_cm):
 
     Results = {}  # All results are stored in this dict. The keys are the name of the classifiers.
     triplet_cm_lists = {}
+    config.kfolds = None
+    
     if config.train_neuralnet:
         transformers = getDeepTransformers()
         ###TripletNetwork + BaseClassifier Experiments:###
